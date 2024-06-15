935c92d528639887aee8105beaddf37430993631
Jamie H Pham <jpham1109@gmail.com>
2024-06-14 22:30:00 -0400

    Add mobileMenu for NavBar and adjusted breakpoints

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import {
@@ -11,8 +11,12 @@ import {
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
+	// state for mobile menu
+	const [menuOpen, setMenuOpen] = useState(false)
+
 	const currentUser = useSelector(selectCurrentUser)
 	const token = useSelector(selectCurrentUsersToken)
+
 	const dispatch = useDispatch()
 	const navigate = useNavigate()
 
@@ -34,40 +38,81 @@ const NavBar = () => {
 		}
 	}, [dispatch, navigate])
 
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen)
+	}
+	const closeMenu = () => {
+		setMenuOpen(false)
+	}
+
 	return (
 		<div className={styles.container}>
 			<Link to="/" className={styles.logo}>
 				<i className="fas fa-cocktail"></i>DRINKEDIN
 			</Link>
 
-			{currentUser ? (
-				<div className={styles.NavLinks}>
-					<Link to="/categories" className={styles.loggedInLink}>
-						Categories
-					</Link>
-					<Link to="/cocktails" className={styles.loggedInLink}>
-						Featured
-					</Link>
-					<Link to="/profile" className={styles.profileLink}>
-						<i className="far fa-user-circle" />
-					</Link>
-					<Link to="/" onClick={handleLogOut} className={styles.loggedInLink}>
-						Logout{' '}
-						{currentUser.full_name
-							? currentUser.full_name
-							: currentUser.username}{' '}
-					</Link>
-				</div>
-			) : (
-				<div className={styles.NavLinks}>
-					<Link to="/login" className={styles.loggedOutLink}>
-						Log In{' '}
-					</Link>
-					<Link to="/signup" className={styles.loggedOutLink}>
-						Sign Up
-					</Link>
-				</div>
-			)}
+			<div className={styles.hamburger} onClick={toggleMenu}>
+				<div className={`${styles.bar} ${menuOpen ? styles.open : ''}`}></div>
+				<div className={`${styles.bar} ${menuOpen ? styles.open : ''}`}></div>
+				<div className={`${styles.bar} ${menuOpen ? styles.open : ''}`}></div>
+			</div>
+			<div className={menuOpen ? styles.mobileMenu : styles.NavLinks}>
+				{currentUser ? (
+					<>
+						<Link
+							to="/categories"
+							className={styles.loggedInLink}
+							onClick={closeMenu}
+						>
+							Categories
+						</Link>
+						<Link
+							to="/cocktails"
+							className={styles.loggedInLink}
+							onClick={closeMenu}
+						>
+							Featured
+						</Link>
+						<Link
+							to="/profile"
+							className={styles.profileLink}
+							onClick={closeMenu}
+						>
+							<i className="far fa-user-circle" />
+						</Link>
+						<Link
+							to="/"
+							onClick={() => {
+								handleLogOut()
+								closeMenu()
+							}}
+							className={styles.loggedInLink}
+						>
+							Logout{' '}
+							{currentUser.full_name
+								? currentUser.full_name
+								: currentUser.username}{' '}
+						</Link>
+					</>
+				) : (
+					<>
+						<Link
+							to="/login"
+							className={styles.loggedOutLink}
+							onClick={closeMenu}
+						>
+							Log In{' '}
+						</Link>
+						<Link
+							to="/signup"
+							className={styles.loggedOutLink}
+							onClick={closeMenu}
+						>
+							Sign Up
+						</Link>
+					</>
+				)}
+			</div>
 		</div>
 	)
 }
